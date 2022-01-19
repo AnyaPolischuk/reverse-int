f7f9fb90f698b454020b300db880d969c4e6ceee
AnyaPolischuk <polisuk092@gmail.com>
2022-01-19 23:17:42 +0300

    feat: did reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let res = ''
+    let str = String(Math.abs(n))
+    for (i = 0; i < str.length; i++) {
+        res = str[i] + res
+    }
+    /*while (n / 10 >= 0) {
+        let lastDigit = n % 10
+        n = n / 10
+
+    }*/
+    return Number(res)
 }
